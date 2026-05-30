f14a8719a4bb6cba271911e82d1ac69d7d81de96
Ktototam228 <voejkovila@gmail.com>
2026-05-30 15:38:02 +0300

    logger.h || класс Logger для записи логов

---
+++ include/logger.h
@@ -0,0 +1,16 @@
+﻿#pragma once
+#include <fstream>
+#include <string>
+#include <chrono>
+#include <ctime>
+
+class Logger {
+public:
+    static void log(const std::string& message) {
+        std::ofstream file("calculator.log", std::ios::app);
+        auto now = std::chrono::system_clock::now();
+        std::time_t now_time = std::chrono::system_clock::to_time_t(now);
+        file << std::ctime(&now_time) << " - " << message << std::endl;
+        file.close();
+    }
+};
